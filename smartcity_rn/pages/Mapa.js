4ede0b02ec710721447a2a91efc0e3ad1875cf9d
Pedro <pedroaraujo_41@hotmail.com>
2020-05-11 16:55:48 +0100

    Commit Final deste branch

--- smartcity_rn/pages/Mapa.js
+++ smartcity_rn/pages/Mapa.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import { View, Text, Button} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View, Button } from 'react-native';
 
 function Mapa ({ navigation }) {
-    return (
-        <View style={{ flex: 1, alginItems: 'center', justifyContent:'center' }}>
-            <Text> Aqui vai ficar o mapa! </Text>
-           
-            <Button
-                color="orange"
-                title="Logout"
-                onPress={ () => navigation.navigate('Login')}
-            />  
-        </View>
-    );
-}
+
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://reactnative.dev/movies.json')
+      .then((response) => response.json())
+      .then((json) => setData(json.movies))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  });
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      {loading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>Ti­tulo: {item.title}, Ano: {item.releaseYear}</Text>
+          )}
+        />
+      )}
+    <Button
+      style={{ marginTop: 10 }}
+      color="orange"
+      title="Logout"
+      onPress={ () => navigation.navigate('Login')}/>
+    </View>
+  );
+
+};
 
 export default Mapa;
