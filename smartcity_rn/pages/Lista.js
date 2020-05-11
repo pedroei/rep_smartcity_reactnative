be069691fa7db1a1389bcf209bbb32a3a4c691fc
Pedro <pedroaraujo_41@hotmail.com>
2020-05-11 22:37:21 +0100

    Lista
    A Listagem é agora feita atraves de funções

--- smartcity_rn/pages/Lista.js
+++ smartcity_rn/pages/Lista.js
@@ -1,137 +1,87 @@
-
-import React, { Component} from 'react';
-import { StyleSheet, Platform, View, Button, Image, Text, TextInput, TouchableOpacity, Alert, YellowBox, ListView, FlatList, TouchableWithoutFeedback, Animated } from 'react-native';
+import React, { Component, useState, useEffect } from 'react';
+import { StyleSheet, Platform, View, Button, TouchableWithoutFeedback, Image, Text, TextInput, TouchableOpacity, Alert, YellowBox, FlatList } from 'react-native';
 
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Swipeable from 'react-native-gesture-handler/Swipeable';
-
 import Realm from 'realm';
-let realm ;
 
-const Stack = createStackNavigator();
+const realm = new Realm({
+  path: 'notas.realm',
+  schema: [{
+    name: 'nota',
+      properties:
+      {
+        id: {type: 'int',   default: 0},
+        titulo: 'string',
+        descricao: 'string',
+        local: 'string',
+        data: 'string'
+      }}]
+});
 
-class Lista extends Component{
-    constructor() {
-        
-        super();
+const query = () => realm.objects('nota');
 
-        realm = new Realm({
-            path: 'notas.realm',
-            schema: [{
-                name: 'nota',
-                properties:
-                {
-                    id: {type: 'int',   default: 0},
-                    titulo: 'string',
-                    descricao: 'string',
-                    local: 'string',
-                    data: 'string'
-                }
-            }]
-        });
-        
-        var notas = realm.objects('nota');
-        this.state = {
-            FlatListItems: notas,
-            id: '',
+function getupdateddata(query) {
+  const [data, setData] = useState(query());
+
+  useEffect(
+      () => {
+        function handleChange(newData) {
+          setData([...newData]);
+        }
+        const dataQuery = query();
+        dataQuery.addListener(handleChange);
+        return () => {
+          dataQuery.removeAllListeners();
         };
+      },
+      [query]
+  );
+  return data;
+}
 
-        
-        realm.addListener('change', () => {
-            this.reloadData();
-            });
+function actionOnRow(item, navigation) {
+    navigation.navigate('EditarNota', item);
+}
 
-    }
+function Lista({ navigation }) {
+  const notas = getupdateddata(query);
+  return(
+     <View style = { styles.MainContainer }>
+         <FlatList
+            data={notas}
+            ItemSeparatorComponent={() => <View style={{ height: 1, width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.2)' }}></View>}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <TouchableWithoutFeedback onPress={ () => actionOnRow(item, navigation)}>
+                    <View style={{ backgroundColor: 'white', padding: 23}}>
+                        {/*<Text>Id: {item.id}</Text>*/}
+                        <Text 
+                        style={{ fontSize: 20, fontWeight: 'bold' }}>
+                        {item.titulo} </Text>
+                        <Text style={{ fontSize: 15}}>{item.local}</Text>
+                        <Text style={{ marginTop: 5 }}>{item.descricao}</Text>
+                        <Text style={{ textAlign: 'right' }}>{item.data}</Text>
+                    </View>
+             </TouchableWithoutFeedback>
+             )}
+         />
+        <View style = { styles.containerBtns }>
+            <TouchableOpacity
+                style = {styles.btn}
+                onPress={ () => navigation.navigate('Login') }>
+                <Text style={styles.text}> Voltar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style = {styles.btn}
+                onPress={ () => navigation.navigate('AddNota') }>
+                <Text style={styles.text}> Adicionar Nota</Text>
+            </TouchableOpacity>
+        </View>
+     </View>
+  );
 
-    reloadData = () => { this.setState({FlatListItems: realm.objects('nota')}); }
-
-    GoToAddNota = () => { this.props.navigation.navigate('AddNota'); }
-    GoBack = () => { this.props.navigation.navigate('Login'); }
-    actionOnRow(item) { this.props.navigation.navigate('EditarNota', item); }
-
-    deleteSwipe = (progress, dragX) => { 
-
-        const scale = dragX.interpolate({
-            inputRange:[-100, 0],
-            outputRange:[1, 0],
-            extrapolate: 'clamp'
-        })
-
-        return(
-            
-                <View style = { styles.containerSwipe }>
-                    <Animated.Text style = { [styles.textSwipe, {transform: [{ scale: scale }] } ] }>Apagar</Animated.Text>
-                </View> 
-            
-        );
-    }
-
-    alertDelete(item) {
-        Alert.alert(
-            'Info',
-            'Tem a certeza que quer apagar este registo?',
-            [
-                { text: 'Não' },
-                {
-                    text: 'Sim',
-                    onPress: () =>{
-                        realm.write(() => {
-                            let task = realm.objects('nota').filtered('id = ' + item.id);
-                            realm.delete(task);
-                            //Esta linha recarrega a pagina, arranjar outra maneira de fechar o swipe!
-                            this.props.navigation.dispatch(StackActions.replace('Lista'))
-                        });
-                    }
-                },
-            ],
-        );
-    }
-
-    render() {
-        
-        return(
-          <View style = { styles.MainContainer }>
-          <FlatList
-                data={this.state.FlatListItems}
-                ItemSeparatorComponent={() => <View style={{ height: 1, width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.2)' }}></View>}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={({ item }) => (
-                    <Swipeable
-                        renderRightActions={this.deleteSwipe}
-                        onSwipeableRightOpen={() => this.alertDelete(item)}
-                    >
-                        <TouchableWithoutFeedback onPress={ () => this.actionOnRow(item)}>
-                            <View style={{ backgroundColor: 'white', padding: 23}}>
-                                {/*<Text>Id: {item.id}</Text>*/}
-                                <Text 
-                                style={{ fontSize: 20, fontWeight: 'bold' }}>
-                                {item.titulo} </Text>
-                                <Text style={{ fontSize: 15}}>{item.local}</Text>
-                                <Text style={{ marginTop: 5 }}>{item.descricao}</Text>
-                                <Text style={{ textAlign: 'right' }}>{item.data}</Text>
-                            </View>
-                        </TouchableWithoutFeedback>
-                    </Swipeable>
-                 )}
-            />
-            <View style = { styles.containerBtns }>
-                <TouchableOpacity
-                    style = {styles.btn}
-                    onPress={ this.GoBack}>
-                    <Text style={styles.text}> Voltar</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style = {styles.btn}
-                    onPress={this.GoToAddNota}>
-                    <Text style={styles.text}> Adicionar Nota</Text>
-                </TouchableOpacity>
-            </View>
-            </View>
-        );
-
-    }
 }
 
 const styles = StyleSheet.create({
