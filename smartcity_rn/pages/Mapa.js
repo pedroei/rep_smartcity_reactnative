0e88bd3d19575d5917621c2613fd9e3401d99f26
Pedro <pedroaraujo_41@hotmail.com>
2020-05-18 21:37:04 +0100

    Teste Login
    Implementado teste de Login

--- smartcity_rn/pages/Mapa.js
+++ smartcity_rn/pages/Mapa.js
@@ -2,41 +2,41 @@ import React, {useEffect, useState} from 'react';
 import {Text, View, Button, Alert, TextInput} from 'react-native';
 
 function Mapa({navigation}) {
-  const [nome, setNome] = useState([]);
   const [email, setEmail] = useState([]);
   const [password, setpassword] = useState([]);
   const [msg, setMsg] = useState([]);
+  const [idUser, setIdUser] = useState([]);
 
-  function registar() {
+  function login() {
     const requestOptions = {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({nome: nome, email: email, password: password}),
+      body: JSON.stringify({email: email, password: password}),
     };
     fetch(
-      'https://pedroacm.000webhostapp.com/cm/cm/index.php/api/registar',
+      'https://pedroacm.000webhostapp.com/cm/cm/index.php/api/login',
       requestOptions,
     )
       .then((response) => response.json())
       .then((data) => {
-        setMsg(data.MSG);
-        if (data.MSG === 'success') {
-          Alert.alert('Registado!');
+        setMsg(data.msg);
+        setIdUser(data.id);
+        if (data.msg === 'success') {
+          Alert.alert('Login!' + data.id);
         } else {
-          Alert.alert(data.MSG);
+          Alert.alert('Email ou Password errados!' + data.id);
         }
       });
   }
 
   return (
     <View style={{flex: 1, padding: 24}}>
-      <TextInput placeholder="Nome" onChangeText={(text) => setNome(text)} />
       <TextInput placeholder="Email" onChangeText={(text) => setEmail(text)} />
       <TextInput
         placeholder="Password"
         onChangeText={(text) => setpassword(text)}
       />
-      <Button title="registar" onPress={registar}></Button>
+      <Button title="login" onPress={login}></Button>
       <Text>{msg}</Text>
     </View>
   );
