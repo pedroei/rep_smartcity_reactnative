c78eac21b9d4b15eb867a73fe732506bf180e96e
Pedro <pedroaraujo_41@hotmail.com>
2020-05-18 23:57:35 +0100

    Mapa Implementado
    Instalação da dependencia necessária e configurações para mostrar o Mapa

--- smartcity_rn/pages/Mapa.js
+++ smartcity_rn/pages/Mapa.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
-import {Text, View, Button, Alert} from 'react-native';
+import {Text, View, Button, Alert, StyleSheet} from 'react-native';
+
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 import {StackActions} from '@react-navigation/native';
 
@@ -7,14 +9,33 @@ function Mapa({route, navigation}) {
   const {id} = route.params;
 
   return (
-    <View style={{flex: 1, padding: 24}}>
-      <Text>{id}</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}></MapView>
       <Button
         title="Logout"
         onPress={() => navigation.dispatch(StackActions.replace('Login'))}
       />
+      <Text style={{fontSize: 24}}>{id}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: '100%',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default Mapa;
