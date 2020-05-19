347f140a0025c243059d98435be9a62b80c13c2f
Pedro <pedroaraujo_41@hotmail.com>
2020-05-19 19:55:36 +0100

    Floating buttons
    Implementados 2 floatting buttons, um para o logout e outro que vai, futuramente, redirecionar para a lista de pontos do utilizador.

--- smartcity_rn/pages/Mapa.js
+++ smartcity_rn/pages/Mapa.js
@@ -1,5 +1,13 @@
 import React, {useState} from 'react';
-import {Text, View, Button, Alert, StyleSheet} from 'react-native';
+import {
+  Text,
+  View,
+  Button,
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
@@ -7,6 +15,10 @@ import {StackActions} from '@react-navigation/native';
 
 function Mapa({route, navigation}) {
   const {id} = route.params;
+
+  function actionButtuon() {
+    Alert.alert('Lista de Pontos!');
+  }
 
   return (
     <View style={styles.container}>
@@ -19,10 +31,23 @@ function Mapa({route, navigation}) {
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}></MapView>
-      <Button
-        title="Logout"
+
+      <TouchableOpacity onPress={actionButtuon} style={styles.btnListaDireita}>
+        <Image
+          source={require('./../images/list.png')}
+          style={styles.FloatingButtonStyle}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity
         onPress={() => navigation.dispatch(StackActions.replace('Login'))}
-      />
+        style={styles.btnListaEsquerda}>
+        <Image
+          source={require('./../images/logout.png')}
+          style={styles.FloatingButtonStyle}
+        />
+      </TouchableOpacity>
+
       <Text style={{fontSize: 24}}>{id}</Text>
     </View>
   );
@@ -35,6 +60,31 @@ const styles = StyleSheet.create({
   },
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  btnListaDireita: {
+    position: 'absolute',
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 30,
+    bottom: 30,
+  },
+  btnListaEsquerda: {
+    position: 'absolute',
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    left: 30,
+    bottom: 30,
+  },
+
+  FloatingButtonStyle: {
+    resizeMode: 'contain',
+    width: 35,
+    height: 35,
+    //backgroundColor:'black'
   },
 });
 
